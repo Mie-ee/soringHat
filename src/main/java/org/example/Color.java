96e1e58cb74dd7d3ecadaf07bf19740b2cf1f9bb
Mie-ee <146546228+Mie-ee@users.noreply.github.com>
2023-10-11 18:44:04 +0900

    Update Color.java

--- src/main/java/org/example/Color.java
+++ src/main/java/org/example/Color.java
@@ -1,12 +1,12 @@
 package org.example;
 
-final public class Color {
-    static String red = "\u001b[00;31m";
-    static String green = "\u001b[00;32m";
-    static String yellow = "\u001b[00;33m";
-    static String purple = "\u001b[00;34m";
-    static String pink = "\u001b[00;35m";
-    static String cyan = "\u001b[00;36m";
-    static String end = "\u001b[00m";
-    static String back = "\u001b[00;47m";
+public class Color {
+    public final static String red = "\u001b[00;31m";
+    public final static String green = "\u001b[00;32m";
+    public final static String yellow = "\u001b[00;33m";
+    public final static String purple = "\u001b[00;34m";
+    public final static String pink = "\u001b[00;35m";
+    public final static String cyan = "\u001b[00;36m";
+    public final static String end = "\u001b[00m";
+    public final static String back = "\u001b[00;47m";
 }
